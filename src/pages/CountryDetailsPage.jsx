8383814a071efe41f5f6386733f8374ee9d01636
Gábor Farkas <farkas.gz@gmail.com>
2023-07-30 22:33:09 +0200

    its too late for unique keys

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,63 @@
-function CountryDetails() {}
+import { Link, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+const countriesUrl = "https://ih-countries-api.herokuapp.com/countries"
+
+function CountryDetails() {
+    const [country, setCountry] = useState([])
+    const [loading, setLoading] = useState(true)
+    const params = useParams()
+
+    useEffect(() => {
+        axios.get(countriesUrl).then((response) => {
+            setCountry(response.data.filter(elem => elem.alpha3Code === params.countryId))
+            setLoading(false)
+        })
+    }, [params])
+
+    return (
+        <div className="container">
+            <p style={{fontSize: "24px", fontWeight: "bold"}}>Country Details</p>
+            {loading && <p>Loading</p>}
+            {
+                country.map((elem) => {
+                    return (
+                        <>
+                            <h1>{elem.name.official}</h1>
+                            <table className="table">
+                                <thead></thead>
+                                <tbody>
+                                    <tr>
+                                        <td style={{width: "30%"}}>Capital</td>
+                                        <td>{elem.capital[0]}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Area</td>
+                                        <td>
+                                            {elem.area} km
+                                            <sup>2</sup>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Borders</td>
+                                        <td>
+                                            <ul>
+                                                {elem.borders.map((element) => {
+                                                    return (
+                                                        <Link to={`../${element}`}>{element}</Link>
+                                                    )
+                                                })}
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </>
+                    )
+                })
+            }
+        </div>
+    )
+}
 
 export default CountryDetails;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,34 @@
-function HomePage() {}
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+const countriesUrl = "https://ih-countries-api.herokuapp.com/countries"
+
+function HomePage() {
+    const [countries, setCountries] = useState([])
+    const [fetching, setFetching] = useState(true)
+
+    useEffect(() => {
+        axios.get(countriesUrl).then((response) => {
+            setCountries(response.data)
+            setFetching(false)
+        })
+    }, [])
+    
+    return (
+        <div className="container" style={{maxHeight: "90vh", overflow: "scroll"}}>
+            <h1 style={{fontSize: "24px"}}>WikiCountries: Your Guide to the World</h1>
+            <div className="list-group">
+                {fetching && <p>Loading</p>}
+                {
+                    countries.map((country) => {
+                        return (
+                            <Link className="list-group-item list-group-item-action" to={country.alpha3Code}><img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt={country.name.common} />{country.name.common}</Link>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
 
 export default HomePage;
